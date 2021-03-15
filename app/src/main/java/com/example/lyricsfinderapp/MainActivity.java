cc4dcbb2fb69244fec25c443a380776cfc410a42
Annish Vykera <avykera@gmail.com>
2021-03-15 14:42:22 +0530

    Final Commit

--- app/src/main/java/com/example/lyricsfinderapp/MainActivity.java
+++ app/src/main/java/com/example/lyricsfinderapp/MainActivity.java
@@ -9,6 +9,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText edt_Artist_Name, edt_Song_Name;
@@ -31,8 +43,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(getApplicationContext(), "button tapped", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), "button tapped", Toast.LENGTH_SHORT).show();
+                String url = "https://api.lyrics.ovh/v1/" + edt_Artist_Name.getText().toString() + "/" + edt_Song_Name.getText().toString();
+                url.replace(" ","20%");
+                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
 
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+                            txt_Lyrics.setText(response.getString("lyrics"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                                Toast.makeText(getApplicationContext(),"Song not found",Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                requestQueue.add(jsonObjectRequest);
             }
         });
 
