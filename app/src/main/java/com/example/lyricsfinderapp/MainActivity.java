e237e1bde292ae3f0217a6ea0d47f75b7691217c
Annish Vykera <avykera@gmail.com>
2021-03-15 13:10:01 +0530

    second commit

--- app/src/main/java/com/example/lyricsfinderapp/MainActivity.java
+++ app/src/main/java/com/example/lyricsfinderapp/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.lyricsfinderapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edt_Artist_Name, edt_Song_Name;
+    Button btn_Get_Lyrics;
+    TextView txt_Lyrics;
+
+    // https://api.lyrics.ovh/v1/artist/title
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edt_Artist_Name = findViewById(R.id.edt_Artist_Name);
+        edt_Song_Name = findViewById(R.id.edt_Song_Name);
+        btn_Get_Lyrics = findViewById(R.id.btn_Get_Lyrics);
+        txt_Lyrics = findViewById(R.id.txt_Lyrics);
+
+        btn_Get_Lyrics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "button tapped", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
